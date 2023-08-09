f14f59d459a9ea5413f1de7b36d60bc552e97083
pankajW <pankajwadhwa007@gmail.com>
2023-08-09 13:20:44 +0530

    Initial Commit

---
+++ PWChatGPT/PWChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  PWChatGPTApp.swift
+//  PWChatGPT
+//
+//  Created by Pankaj Wadhwa on 09/08/23.
+//
+
+import SwiftUI
+
+@main
+struct PWChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
